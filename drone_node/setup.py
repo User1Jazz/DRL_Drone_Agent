9a5c595ca1e8b3b6dd86cfc2dfb9e7d711d892ea
User1Jazz <98923661+User1Jazz@users.noreply.github.com>
2024-02-01 20:37:39 +0000

    Added initial files

---
+++ drone_node/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'drone_node'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='blue02',
+    maintainer_email='blue02@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'drone_agent = drone_node.drone_agent:main',
+        ],
+    },
+)
