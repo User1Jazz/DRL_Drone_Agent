ed63f7688ab4c3d91ebfe1ba65f583ace3420480
User1Jazz <phoenix.productions222@gmail.com>
2024-02-02 18:45:29 +0000

    Fixed message types and imports

--- drone_node/drone_agent.py
+++ drone_node/drone_agent.py
@@ -1,77 +1,47 @@
 import numpy as np
 from tensorflow import keras
 
-# Manual control dependencies
-import keyboard
-
 # ROS2 dependencies
 import rclpy
 from rclpy.node import Node
 from drone_sim_messages.msg import DroneControl
 from drone_sim_messages.msg import DroneSensors
 
-class DroneAgent:
+class DroneAgent(Node):
   def __init__(self, drone_id):
     # ROS2 Setup BELOW!
     super().__init__('drone_agent')    
     # Publishing stuff
     pub_topic = "/" + drone_id + "/cmd"
-    self.publisher_ = self.create_publisher(String, pub_topic, 10)
+    self.publisher_ = self.create_publisher(DroneControl, pub_topic, 10)
     timer_period = 0.5  # seconds
     self.timer = self.create_timer(timer_period, self.timer_callback)
     self.i = 0
     # Subscription stuff
-    sub_topic = "/" + drone_id + "/sensors"
+    sub_topic = "/" + drone_id + "/data"
     self.subscription = self.create_subscription(
-        String,
+        DroneSensors,
         sub_topic,
         self.listener_callback,
         10)
     self.subscription  # prevent unused variable warning
     # ROS2 Setup ABOVE!
-
+    
     # Setup the agent now!
-
-    return
 
   # Send drone control data
   def timer_callback(self):
     msg = DroneControl()
-    msg.twist.linear.x = 0
-    msg.twist.linear.y = 0
-    msg.twist.linear.z = 0
-    msg.twist.angular.x = 0
-    msg.twist.angular.y = 0
-    msg.twist.angular.z = 0
-
-    # Forward/Backward
-    if keyboard.is_pressed('w'):
-       msg.twist.linear.x = 1
-    if keyboard.is_pressed('s'):
-       msg.twist.linear.x = -1
-    
-    # Left/Right
-    if keyboard.is_pressed('a'):
-       msg.twist.linear.y = -1
-    if keyboard.is_pressed('d'):
-       msg.twist.linear.y = 1
-    
-    # Up/Down
-    if keyboard.is_pressed('r'):
-       msg.twist.linear.z = 1
-    if keyboard.is_pressed('f'):
-       msg.twist.linear.z = -1
-    
-    #Yaw Left/Right
-    if keyboard.is_pressed('e'):
-       msg.twist.angular.z = 1
-    if keyboard.is_pressed('q'):
-       msg.twist.angular.z = -1
+    msg.twist.linear.x = 1.0
+    msg.twist.linear.y = 0.0
+    msg.twist.linear.z = 1.0
+    msg.twist.angular.x = 0.0
+    msg.twist.angular.y = 0.0
+    msg.twist.angular.z = 0.0
 
     self.get_logger().info("Sending control data")
     self.i += 1
     self.publisher_.publish(msg)
-    return
 
   # Listen to incoming data; This function should update the observation data
   def listener_callback(self, msg):
@@ -79,16 +49,14 @@ class DroneAgent:
     self.imu_data = np.array([0.0,
                               0.0,
                               0.0,
-                              msg.imu.angular_velocity.x,
-                              msg.imu.angular_velocity.y,
-                              msg.imu.angular_velocity.z,
+                              msg.angular_velocity.x,
+                              msg.angular_velocity.y,
+                              msg.angular_velocity.z,
                               msg.linear_acceleration.x,
                               msg.linear_acceleration.y,
                               msg.linear_acceleration.z])             # (pitch, roll, yaw, linear acceleration x 3, angular velocity x 3)
     self.height_data = np.array([msg.height])                         # Single value for height
     self.target_position = np.array([0.0, 0.0, 0.0])                  # X, Y and Z coordinates for target position
-    
-    return
 
 # Main function
 def main(args=None):
